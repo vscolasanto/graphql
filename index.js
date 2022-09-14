ce449445b9a012f3f1f6851d79c80628fd4491e2
Vinicius Colasanto <vscolasanto@gmail.com>
2022-09-14 14:14:09 -0300

    feat: add resolver salarioComImposto

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 import { ApolloServer, gql } from 'apollo-server';
 
+const currencyBRL = (value) => {
+  return value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+};
+
 const typeDefs = gql`
   scalar Date
 
+  type Usuario {
+    id: ID
+    nome: String!
+    email: String!
+    idade: Int
+    salario: String
+    salarioComImposto: String
+    vip: Boolean
+  }
+
   # API entrance
   type Query {
-    today: String
-    timeNow: String
-    date: Date
+    today: String!
+    timeNow: String!
+    date: Date!
+    usuarioLogado: Usuario
   }
 `;
 
 const resolvers = {
+  Usuario: {
+    salario(user) {
+      return currencyBRL(user.salarioReal);
+    },
+    salarioComImposto(user) {
+      return currencyBRL(user.salarioReal * 1.272);
+    },
+  },
   Query: {
     today() {
       const date = new Date();
@@ -23,6 +46,16 @@ const resolvers = {
     },
     date() {
       return new Date();
+    },
+    usuarioLogado() {
+      return {
+        id: 1,
+        nome: 'Vinicius',
+        email: 'vscolasanto@gmail.com8',
+        idade: 18,
+        salarioReal: 22970.43,
+        vip: true,
+      };
     },
   },
 };
