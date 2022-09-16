d801e214b1749759ee707f10733541361d535088
Vinicius Colasanto <vscolasanto@gmail.com>
2022-09-15 23:52:21 -0300

    feat: return an array of users

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 import { ApolloServer, gql } from 'apollo-server';
+import { faker } from '@faker-js/faker';
+faker.locale = 'pt_BR';
 
 const currencyBRL = (value) => {
   return value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
 };
 
+const generateUser = () => ({
+  id: faker.datatype.uuid(),
+  name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+  email: faker.internet.email(),
+  age: faker.datatype.number(80),
+  realSalary: faker.finance.amount(21000, 25000, 2),
+  taxSalary: faker.finance.amount(27000, 35000, 2),
+  vip: faker.datatype.boolean(),
+});
+
 const typeDefs = gql`
   scalar Date
 
-  type Usuario {
+  type User {
     id: ID
-    nome: String!
+    name: String!
     email: String!
-    idade: Int
-    salario: String
-    salarioComImposto: String
+    age: Int
+    salary: String
+    taxSalary: String
     vip: Boolean
   }
 
-  type Produto {
-    nome: String!
-    preco: Float!
-    desconto: Float
-    precoComDesconto: String
+  type Product {
+    name: String!
+    price: Float!
+    discount: Float
+    discountPrice: String
   }
 
   # API entrance
@@ -29,23 +41,25 @@ const typeDefs = gql`
     today: String!
     timeNow: String!
     date: Date!
-    usuarioLogado: Usuario
-    produtoEmDestaque: Produto
+    loggedUser: User
+    featuredProduct: Product
+    lotteryNumbers: [Int!]!
+    getUsers: [User!]!
   }
 `;
 
 const resolvers = {
-  Usuario: {
-    salario(user) {
-      return currencyBRL(user.salarioReal);
+  User: {
+    salary(user) {
+      return currencyBRL(user.realSalary);
     },
-    salarioComImposto(user) {
-      return currencyBRL(user.salarioReal * 1.272);
+    taxSalary(user) {
+      return currencyBRL(user.realSalary * 1.272);
     },
   },
-  Produto: {
-    precoComDesconto(produto) {
-      return currencyBRL(produto.preco - produto.preco * (produto.desconto / 100));
+  Product: {
+    discountPrice(product) {
+      return currencyBRL(product.price - product.price * (product.discount / 100));
     },
   },
   Query: {
@@ -60,22 +74,38 @@ const resolvers = {
     date() {
       return new Date();
     },
-    usuarioLogado() {
+    loggedUser() {
       return {
         id: 1,
-        nome: 'Vinicius',
+        name: 'Vinicius',
         email: 'vscolasanto@gmail.com8',
-        idade: 18,
-        salarioReal: 22970.43,
+        age: 18,
+        realSalary: 22970.43,
         vip: true,
       };
     },
-    produtoEmDestaque() {
+    featuredProduct() {
       return {
-        nome: 'Playstation 5',
-        preco: 5900.9,
-        desconto: 10,
+        name: 'Playstation 5',
+        price: 5900.9,
+        discount: 10,
       };
+    },
+    lotteryNumbers() {
+      return [
+        ...new Set(
+          Array(15)
+            .fill(0)
+            .map(() => parseInt(Math.random() * 60 + 1))
+        ),
+      ]
+        .slice(0, 6)
+        .sort((a, b) => a - b);
+    },
+    getUsers() {
+      let users = [];
+      Array.from({ length: 50 }).forEach(() => users.push(generateUser()));
+      return users;
     },
   },
 };
